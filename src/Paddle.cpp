f2c707f93effcfee663213da462f08fdfef0f61f
Colton Ward <wardc0714@outlook.com>
2025-03-21 00:29:23 -0500

    paddles bounce (kind of)

--- src/Paddle.cpp
+++ src/Paddle.cpp
@@ -2,24 +2,63 @@
 
 #include "World.hpp"
 
+#include "Ball.hpp"
+
 using namespace glm;
 
-void Paddle::Start() {
+void Paddle::Start()
+{
     scale = vec3(20.0f, 100.0f, 0.0f);
 }
 
-void Paddle::Update(float _dt) {
+void Paddle::Update(float _dt)
+{
     vec2 dir;
 
     if (name == "LeftPaddle")
     {
+
         dir.y += inputManager->GetKey(SDL_SCANCODE_W);
         dir.y += inputManager->GetKey(SDL_SCANCODE_S) * -1;
+        Ball *ball = world->FindByName<Ball>("Ball");
+        if (EntityOverlap2D(*this, *ball))
+        {
+            if (!overlap_l)
+            {
+                overlap_l = true;
+            }
+        }
+        if (overlap_l)
+        {
+            position.x -= 5.0f;
+            if (position.x < -10.0f)
+            {
+                overlap_l = false;
+                position.x = 5.0f;
+            }
+        }
     }
     else if (name == "RightPaddle")
     {
         dir.y += inputManager->GetKey(SDL_SCANCODE_UP);
         dir.y += inputManager->GetKey(SDL_SCANCODE_DOWN) * -1;
+        Ball *ball = world->FindByName<Ball>("Ball");
+        if (EntityOverlap2D(*this, *ball))
+        {
+            if (!overlap_r)
+            {
+                overlap_r = true;
+            }
+        }
+        if (overlap_r)
+        {
+            position.x += 5.0f;
+            if (position.x > 645.0f)
+            {
+                overlap_r = false;
+                position.x = 635.0f;
+            }
+        }
     }
 
     position.y += dir.y * speed * _dt;
@@ -30,7 +69,8 @@ void Paddle::Update(float _dt) {
         position.y = scale.y * 0.5f;
 }
 
-void Paddle::Draw() {
+void Paddle::Draw()
+{
     mat4 transform = mat4(1.0f);
     transform = translate(transform, position);
     transform = glm::scale(transform, scale);
@@ -40,6 +80,6 @@ void Paddle::Draw() {
     shader.SetMat4("TRANSFORM", transform);
 }
 
-void Paddle::OnDestroy() {
-    
+void Paddle::OnDestroy()
+{
 }
--- src/Paddle.hpp
+++ src/Paddle.hpp
@@ -10,6 +10,8 @@ public:
     void Update(float _dt);
     void Draw();
     void OnDestroy();
+    bool overlap_l = false;
+    bool overlap_r = false;
 
     float speed = 200.0f;
 };
